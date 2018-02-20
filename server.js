8e55f509d5c40c34b9056bbcdb75e1041be114ea
Glitch (opalescent-needle) <none>
2018-02-20 16:23:52 +0000

    📅⚓️ Checkpoint server.js:692490/187

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Bing = require('node-bing-api')({ accKey: ''});
+const Bing = require('node-bing-api')({ accKey: 'd1abd3c3f4454352909ccd226bfa59ae'});
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(process.cwd() + '/views'));
 
+// Routes
+app.get('api/imagesearch/:search*', (req, res, next) => {
+  let search req.params;
+});
+
 app.listen(process.env.PORT, () => {
   console.log('Node.js listening ...');
 });
-
-// Routes
