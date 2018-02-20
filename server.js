e3bf1bfca100d9f07d9d24abb05b48804088e55b
Glitch (opalescent-needle) <none>
2018-02-20 16:13:47 +0000

    🍺🏍 Checkpoint server.js:692490/95

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Bing = require('node-bing-api')({ accKey: ''});
 const app = express();
 
 app.use(cors());
