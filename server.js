52e4286705dd50bb5813c4558449330b949e8430
Glitch (opalescent-needle) <none>
2018-02-20 20:21:02 +0000

    🔖👵 Checkpoint server.js:692490/38 package.json:692490/3

--- server.js
+++ server.js
@@ -5,19 +5,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 const searchTerm = require('./models/searchTerm');
-mongoose.connect(process.env.MONGOLAB_URI);
+// mongoose.connect(process.env.MONGOLAB_URI);
 
-// AIzaSyBil5Y0mMTsPipexN_mCVupYcrzXg04Fi8 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(process.cwd() + '/views'));
 
 function get(url, callback) {
   https.get(url, (res) => {
-    if (res.statusCode !== 200) {
-      callback(res.statusCode, 'Error: could not get resource');
-      return;
-    }
+  
     
     let data = '';
     res.on('data', (chunk) => {
@@ -51,7 +46,7 @@ app.get('/api/imagesearch/:search*', (req, res, next) => {
     }
   });
   
-  get('https://api.qwant.com/api/search/images?count=10&offset=1&q=cars', (statusCode, data) => {
+  get('https://api.qwant.com/api/search/images?count=10&offset=1&q=cats', (statusCode, data) => {
     console.log(statusCode, data);
     res.end();
   });
